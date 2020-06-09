2b0b789ac95be3eac6076d2b72929c1b585ae953
vtadevosian <vahram_tadevosyan@edu.aua.am>
2020-06-09 15:34:05 +0400

    Initial Commit

---
+++ xylo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xylo
+//
+//  Created by Vahram Tadevosian on 6/9/20.
+//  Copyright © 2020 Vahram Tadevosian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
