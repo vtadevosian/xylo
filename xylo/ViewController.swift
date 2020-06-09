7cb45d6cf3876d2848767070b52380c5c1e7daf9
vtadevosian <vahram_tadevosyan@edu.aua.am>
2020-06-09 18:05:21 +0400

    Add the xylophone key sound files and the UI.

--- xylo/ViewController.swift
+++ xylo/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let unhighlightedKeyColors = [#colorLiteral(red: 1, green: 0.2941176471, blue: 0, alpha: 1), #colorLiteral(red: 1, green: 0.6705882353, blue: 0, alpha: 1), #colorLiteral(red: 1, green: 0.9001885056, blue: 0, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 0, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 0.4442569613, alpha: 1), #colorLiteral(red: 0, green: 1, blue: 0.9613726735, alpha: 1), #colorLiteral(red: 0, green: 0.576669395, blue: 1, alpha: 1), #colorLiteral(red: 0.5466651917, green: 0, blue: 1, alpha: 1)]
+    let highlightedKeyColors = [#colorLiteral(red: 0.7907230258, green: 0.2318771482, blue: 0.03286046535, alpha: 1), #colorLiteral(red: 0.8281216025, green: 0.5575735569, blue: 0.05037004501, alpha: 1), #colorLiteral(red: 0.8195382953, green: 0.7401053905, blue: 0.05982712656, alpha: 1), #colorLiteral(red: 0.04548472166, green: 0.7471590638, blue: 0.05141060054, alpha: 1), #colorLiteral(red: 0.07161668688, green: 0.6606044173, blue: 0.2965246439, alpha: 1), #colorLiteral(red: 0.03887887299, green: 0.6973400116, blue: 0.6706439257, alpha: 1), #colorLiteral(red: 0.02827665024, green: 0.4195930958, blue: 0.7308012843, alpha: 1), #colorLiteral(red: 0.3569839597, green: 0.01296158601, blue: 0.6559349895, alpha: 1)]
+
+    @IBOutlet var keyButtons: [XyloKeyButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(keyButtons)
+        setUpKeys()
     }
-
-
+    
+    private func setUpKeys() {
+        for i in 0..<8 {
+            keyButtons[i].unhighlightedBGColor = unhighlightedKeyColors[i]
+            keyButtons[i].highlightedBGColor = highlightedKeyColors[i]
+        }
+    }
 }
+
 
